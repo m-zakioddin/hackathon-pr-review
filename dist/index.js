1ae30bcbd683d9bc15c0030f69b134e3846c724c
mzakioddin <mzakioddin@entrata.com>
2024-05-20 16:39:54 +0530

    dist

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+function abc() {
+  console.log(" abc function called---------------");
+}
+
+function test() {
+  console.log(" test function called---------------");
+}
+
+function demo() {
+  console.log(" demo function called---------------");
+}
